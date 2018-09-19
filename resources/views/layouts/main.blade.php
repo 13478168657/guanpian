855e78160aa0a26bb3e13ccf4bae8369e7d45693
ykk <ykk@qq.com>
2018-09-19 23:03:47 +0800

    co

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,8 +5,6 @@
 	<title>六安瓜片网是六安瓜片原产地官方网站，为宣传原产地的六安瓜片品牌、六安瓜片价格，服务茶农茶企</title>
 	<meta name="keywords" content="六安瓜片,六安瓜片价格,六安瓜片原产地,白茶文化,白茶种植,白茶历史">
 	@yield('title')
-	<meta name="description"
-		  content="六安瓜片网是由六安县人民政府六安县白茶产业发展办公室、六安瓜片协会主办的官方网站， 六安瓜片企业与广大消费者搭建了信用交易平台，提供最新六安瓜片价格，传递有关六安瓜片产业发展背景和其特有的品质，确保消费者购买真正原产地的六安瓜片，促进行业良性发展。">
 	<link rel="stylesheet" href="/css/base.css">
 	<link rel="stylesheet" href="/css/index.css">
 	<link rel="stylesheet" href="/css/normalize.css">
