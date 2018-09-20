fd1be7247b9ddfeb12092621c328241281341d3f
ykk <ykk@qq.com>
2018-09-20 09:31:30 +0800

    添加瓜片

--- resources/views/home/imgList.blade.php
+++ resources/views/home/imgList.blade.php
@@ -1,211 +1,53 @@
-<!doctype html>
-<html lang="en">
-<head>
-    @section('title')
-        @if($page>=2)
-            <title>{{$category->meta_title}}-六安瓜片网-第{{$page}}页</title>
-        @else
-            <title>{{$category->meta_title}}-六安瓜片网</title>
-        @endif
-        <meta name="keywords" content="{{$category->meta_keyword}}" />
-        <meta name="description" content="{{$category->meta_description}}" />
-    @endsection
-    <link rel="stylesheet" href="css/base.css">
-    <link rel="stylesheet" href="css/index.css">
-    <link rel="stylesheet" href="css/normalize.css">
-    <link rel="stylesheet" href="css/lunbotu.css">
-    <link href="css/baicha.css" rel="stylesheet" type="text/css" />
-    <link rel="stylesheet" rev="stylesheet" href="css/global.css" type="text/css" media="all" />
-    <link rel="stylesheet" rev="stylesheet" href="css/listglobal.css" type="text/css" media="all" />
-    <script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
-    <script>
-        $(function(){
-            $('#friendlinks i:last').remove();
-            $('.slides li:first div:gt(4)').appendTo($('.slides li:eq(1)'));
-        })
-        function randomsort(a, b) {
-            return Math.random()>.5 ? -1 : 1;
-        }
-    </script>
-</head>
-<body>
-<div class="header-top">
-    <div class="header-top-c w">
-        <ul>
-            <li><a href="">六安瓜片网</a></li>
-            <span>|</span>
-            <li><a href="">六安瓜片网</a></li>
-            <span>|</span>
-            <li><a href="">六安瓜片网</a></li>
-            <span>|</span>
-            <li><a href="">六安瓜片网</a></li>
-            <span>|</span>
-            <li><a href="">六安瓜片网</a></li>
-            <span>|</span>
-            <li><a href="">六安瓜片网</a></li>
-        </ul>
-    </div>
-</div>
-<div class="header clearfix">
-    <div class="w">
-        <div class="logo">
-            <a href=""><img src="images/logo.png" alt="六安瓜片网" title="六安瓜片网"></a>
-        </div>
-        <div class="logo-left">
-            <a href=""><img src="images/luntan_bg.png" alt="六安瓜片网" title="六安瓜片网"></a>
-        </div>
-        <div class="logo-right">
-            <a href=""><img style="width:100px;height:100px;" src="images/weixin.png" alt="六安瓜片网"></a>
-        </div>
-    </div>
-</div>
-<div class="nav">
-    <div class="nav-c w">
-        <ul>
-            <li><a href="" class="ziti">网站首页</a></li>
-            <li><a href="">新闻资讯</a></li>
-            <li><a href="">瓜片价格</a></li>
-            <li><a href="">瓜片冲泡方法</a></li>
-            <li><a href="">瓜片功效</a></li>
-            <li><a href="">茶叶购买鉴别</a></li>
-            <li><a href="">瓜片种植</a></li>
-            <li><a href="">六安瓜片图片</a></li>
-            <li><a href="">瓜片品牌</a></li>
-            <li><a href="">六安瓜片简介</a></li>
-        </ul>
-    </div>
-</div>
+@extends('layouts.main')
+@section('title')
+    @if($page>=2)
+        <title>{{$category->meta_title}}-六安瓜片网-第{{$page}}页</title>
+    @else
+        <title>{{$category->meta_title}}-六安瓜片网</title>
+    @endif
+    <meta name="keywords" content="{{$category->meta_keyword}}" />
+    <meta name="description" content="{{$category->meta_description}}" />
+@endsection
+@section('css')
+    <link rel="stylesheet" href="/css/list.css">
+    <link rel="stylesheet" href="/css/details.css">
+    <link href="/css/baicha.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" rev="stylesheet" href="/css/global.css" type="text/css" media="all" />
+    <link rel="stylesheet" rev="stylesheet" href="/css/listglobal.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="/css/lunbotu.css">
+@endsection
 
-
+@section('content')
+<script>
+    $(function(){
+        $('#friendlinks i:last').remove();
+        $('.slides li:first div:gt(4)').appendTo($('.slides li:eq(1)'));
+    })
+    function randomsort(a, b) {
+        return Math.random()>.5 ? -1 : 1;
+    }
+</script>
 <!--breadCrumbs_begin-->
 <div id="breadCrumbs">
-    <a href="../" target="_self" title="图片" class="CurrChnlCls">图片</a>&nbsp;&gt;&nbsp;<a href="./" target="_self" title="热图" class="CurrChnlCls">热图</a>
+    <a href="/" target="_self" title="首页" class="CurrChnlCls">首页</a>&nbsp;&gt;&nbsp;<a href="/index{{$category->id}}.html" target="_self" title="{{$category->name}}" class="CurrChnlCls">{{$category->name}}</a>
 </div>
 <!--breadCrumbs_end-->
 <!--main_begin-->
 <div id="main" class="picList noRightS">
     <ul class="list">
-
-        <li><a href="./201809/t20180917_12061035.htm" target="_self"><img src="images/w020180917573280758597.jpg" alt="PC.jpg"/></a><h2><a href="http://www.taiwan.cn/tp/rw/201809/t20180917_12061035.htm" target="_self" title="台风袭击菲律宾已致60余死 恐有数十人被泥石流掩埋">台风袭击菲律宾已致60余死 恐有数十人被泥石流掩埋</a></h2><p class="info"><span>2018-09-17</span></p></li>
-
-        <li><a href="./201809/t20180913_12059164.htm" target="_self"><img src="images/w020180913353434679269.jpg" alt="PC.jpg"/></a><h2><a href="http://www.taiwan.cn/tp/rw/201809/t20180913_12059164.htm" target="_self" title="苹果秋季新品发布会举行 双卡双待iPhone问世">苹果秋季新品发布会举行 双卡双待iPhone问世</a></h2><p class="info"><span>2018-09-13</span></p></li>
-
-        <li><a href="./201809/t20180912_12058659.htm" target="_self"><img src="images/w020180912485808894615.jpg" alt="PC.jpg"/></a><h2><a href="http://www.taiwan.cn/tp/rw/201809/t20180912_12058659.htm" target="_self" title="金正恩会见韩国国家安全主任郑英永">金正恩会见韩国国家安全主任郑英永</a></h2><p class="info"><span>2018-09-12</span></p></li>
-
-        <li><a href="./201809/t20180910_12057752.htm" target="_self"><img src="images/w020180910538621038611.jpg" alt="IMG_0962_副本.jpg"/></a><h2><a href="http://www.taiwan.cn/tp/rw/201809/t20180910_12057752.htm" target="_self" title="两岸记者三晋行：同煤集团塔山煤矿参访交流">两岸记者三晋行：同煤集团塔山煤矿参访交流</a></h2><p class="info"><span>2018-09-10</span></p></li>
-
-        <li><a href="./201809/t20180910_12057734.htm" target="_self"><img src="images/w020180910526873321780.jpg" alt="GOPR0741_副本.jpg"/></a><h2><a href="http://www.taiwan.cn/tp/rw/201809/t20180910_12057734.htm" target="_self" title="两岸记者三晋行：塔山光伏电站">两岸记者三晋行：塔山光伏电站</a></h2><p class="info"><span>2018-09-10</span></p></li>
-
-        <li><a href="./201809/t20180910_12057718.htm" target="_self"><img src="images/w020180910525085913183.jpg" alt="IMG_1117_副本.jpg"/></a><h2><a href="http://www.taiwan.cn/tp/rw/201809/t20180910_12057718.htm" target="_self" title="两岸记者三晋行：兰花、木瓜培育基地">两岸记者三晋行：兰花、木瓜培育基地</a></h2><p class="info"><span>2018-09-10</span></p></li>
-
-        <li><a href="./201809/t20180907_12056992.htm" target="_self"><img src="images/w020180907613024620229.jpg" alt="IMG_1174_副本.jpg"/></a><h2><a href="http://www.taiwan.cn/tp/rw/201809/t20180907_12056992.htm" target="_self" title="两岸记者三晋行：大同市法华寺">两岸记者三晋行：大同市法华寺</a></h2><p class="info"><span>2018-09-07</span></p></li>
-
-        <li><a href="./201809/t20180907_12056990.htm" target="_self"><img src="images/w020180907604055861931.jpg" alt="IMG_1126_副本.jpg"/></a><h2><a href="http://www.taiwan.cn/tp/rw/201809/t20180907_12056990.htm" target="_self" title="两岸记者三晋行：两岸记者参观大同市牧同乳业生产车间">两岸记者三晋行：两岸记者参观大同市牧同乳业生产车间</a></h2><p class="info"><span>2018-09-07</span></p></li>
-
-        <li><a href="./201809/t20180907_12056924.htm" target="_self"><img src="images/w020180907554638128376.jpg" alt="fjt.jpg"/></a><h2><a href="http://www.taiwan.cn/tp/rw/201809/t20180907_12056924.htm" target="_self" title="两岸记者三晋行：大同古城墙">两岸记者三晋行：大同古城墙</a></h2><p class="info"><span>2018-09-07</span></p></li>
-
-        <li><a href="./201809/t20180906_12056441.htm" target="_self"><img src="images/w020180906570954247159.jpg" alt="GOPR0727_副本.jpg"/></a><h2><a href="http://www.taiwan.cn/tp/rw/201809/t20180906_12056441.htm" target="_self" title="山西大同云冈石窟">山西大同云冈石窟</a></h2><p class="info"><span>2018-09-06</span></p></li>
-
-        <li><a href="./201809/t20180906_12056398.htm" target="_self"><img src="images/w020180906554684857930.jpg" alt="IMG_0875_副本.jpg"/></a><h2><a href="http://www.taiwan.cn/tp/rw/201809/t20180906_12056398.htm" target="_self" title="两岸记者三晋行：大同云冈石窟">两岸记者三晋行：大同云冈石窟</a></h2><p class="info"><span>2018-09-06</span></p></li>
-
-        <li><a href="./201809/t20180906_12056385.htm" target="_self"><img src="images/w020180906535756043734.jpg" alt="fuujiantu.jpg"/></a><h2><a href="http://www.taiwan.cn/tp/rw/201809/t20180906_12056385.htm" target="_self" title="两岸记者三晋行：大同日报社">两岸记者三晋行：大同日报社</a></h2><p class="info"><span>2018-09-06</span></p></li>
-
-        <li><a href="./201809/t20180906_12056382.htm" target="_self"><img src="images/w020180906532838852905.jpg" alt="fjt.jpg"/></a><h2><a href="http://www.taiwan.cn/tp/rw/201809/t20180906_12056382.htm" target="_self" title="两岸记者三晋行：玉龙马会">两岸记者三晋行：玉龙马会</a></h2><p class="info"><span>2018-09-06</span></p></li>
-
-        <li><a href="./201809/t20180906_12056379.htm" target="_self"><img src="images/w020180906524204998049.jpg" alt="fjt.jpg"/></a><h2><a href="http://www.taiwan.cn/tp/rw/201809/t20180906_12056379.htm" target="_self" title="两岸记者三晋行：右玉南山森林公园">两岸记者三晋行：右玉南山森林公园</a></h2><p class="info"><span>2018-09-06</span></p></li>
-
-        <li><a href="./201809/t20180906_12056367.htm" target="_self"><img src="images/w020180906515504023426.jpg" alt="fjt.jpg"/></a><h2><a href="http://www.taiwan.cn/tp/rw/201809/t20180906_12056367.htm" target="_self" title="两岸记者三晋行：右玉精神展览馆">两岸记者三晋行：右玉精神展览馆</a></h2><p class="info"><span>2018-09-06</span></p></li>
-
-        <li><a href="./201809/t20180905_12056033.htm" target="_self"><img src="images/w020180905602449545006.jpg" alt="IMG_0633_副本.jpg"/></a><h2><a href="http://www.taiwan.cn/tp/rw/201809/t20180905_12056033.htm" target="_self" title="两岸记者三晋行：右玉县古县城与老城墙">两岸记者三晋行：右玉县古县城与老城墙</a></h2><p class="info"><span>2018-09-05</span></p></li>
-
-        <li><a href="./201809/t20180905_12056025.htm" target="_self"><img src="images/w020180905597219595350.jpg" alt="IMG_0751_副本.jpg"/></a><h2><a href="http://www.taiwan.cn/tp/rw/201809/t20180905_12056025.htm" target="_self" title="两岸记者三晋行：杀虎口与右玉县博物馆">两岸记者三晋行：杀虎口与右玉县博物馆</a></h2><p class="info"><span>2018-09-05</span></p></li>
-
-        <li><a href="./201809/t20180905_12056020.htm" target="_self"><img src="images/w020180905589121591695.jpg" alt="GOPR0672_副本.jpg"/></a><h2><a href="http://www.taiwan.cn/tp/rw/201809/t20180905_12056020.htm" target="_self" title="两岸记者三晋行：杀虎堡堡址">两岸记者三晋行：杀虎堡堡址</a></h2><p class="info"><span>2018-09-05</span></p></li>
-
-        <li><a href="./201809/t20180905_12055934.htm" target="_self"><img src="images/w020180905564505164507.jpg" alt="IMG_0532_副本.jpg"/></a><h2><a href="http://www.taiwan.cn/tp/rw/201809/t20180905_12055934.htm" target="_self" title="两岸记者三晋行：马邑博物馆">两岸记者三晋行：马邑博物馆</a></h2><p class="info"><span>2018-09-05</span></p></li>
-
-        <li><a href="./201809/t20180905_12055885.htm" target="_self"><img src="images/w020180905548503562956.jpg" alt="2.jpg"/></a><h2><a href="http://www.taiwan.cn/tp/rw/201809/t20180905_12055885.htm" target="_self" title="两岸记者三晋行：朔州崇福寺">两岸记者三晋行：朔州崇福寺</a></h2><p class="info"><span>2018-09-05</span></p></li>
-
+        @foreach($articles as $article)
+        <li>
+            <a href="/thread-{{$article->id}}.html" target="_self"><img src="{{env('IMG_URL')}}/{{$article->thumbPic}}" alt="{{$article->title}}"/></a>
+            <h2>
+                <a href="/thread-{{$article->id}}.html" target="_self" title="{{$article->title}}">{{$article->title}}</a>
+            </h2>
+            <p class="info"><span>{{date('Y-m-d',strtotime($article->created_at))}}</span></p></li>
+        @endforeach
     </ul>
-
 </div>
 <div class="pagination">
-    <span class='pagination-curr'>首页</span>
-    <span class='pagination-curr'>上一页</span>
-    <span class='pagination-curr'>下一页</span>
-    <span class='pagination-curr'>尾页</span>
+    <?php
+    echo htmlspecialchars_decode($pageSize);
+    ?>
 </div>
-<!--footer start-->
-<div class="footer-nav">
-    <div class="footer-nav-c w">
-        <span>六安瓜片网</span>&nbsp;&nbsp;<span>永久网址：www.guapian.cn</span>
-    </div>
-    <div id="footer">
-        <table class="table">
-            <tbody>
-            <tr>
-                <th width="360" valign="top">
-                    <table class="wx">
-                        <tbody>
-                        <tr>
-                            <td><img src="images/weixin.png" alt="" width="109" height="109">
-                                <p align="center">扫一扫关注<br>
-                            <td><img src="images/weixin.png" alt="" width="109" height="109">
-                                <p align="center">扫一扫<br>
-                            <td><img src="images/weixin.png" alt="" width="109" height="109">
-                                <p align="center">扫一扫<br>
-                                    <!--<td><img src="/style/2018/images/wx2.jpg" width="109" height="109" alt=""/>
-                                        <p align="center">扫一扫关注<br>
-                                            六安瓜片更多</p></td>-->
-                        </tr>
-                        </tbody>
-                    </table>
-                </th>
-                <th width="560" valign="top">
-                    <dl class="copyright clearfix">
-                        <dt>主办单位</dt>
-                        <dd>六安县农业局<br>
-                            六安瓜片协会　六安县茶文化促进会
-                        </dd>
-                    </dl>
-                    <dl class="copyright clearfix">
-                        <dt>承建运营</dt>
-                        <dd>六安县安信信息有限公司</dd>
-                        <dd>六安瓜片产业服务部</dd>
-                        <dd>联系人：李庭裕 13967247869</dd>
-                    </dl>
-                    <dl class="copyright clearfix">
-                        <dt>茶园证补办</dt>
-                        <dd>地址：六安县六安城北科技创业园5号楼4层408办公室</dd>
-                        <dd>电话：0572-5223705</dd>
-                    </dl>
-                </th>
-                <td class="table_co" valign="top"><h5>联系我们</h5>
-                    <p>浙江省六安县六安城北科技创业园5号楼4层</p>
-                    <p>服务热线：0572-5223705</p>
-                    <p>合作电话：0572-5220701</p>
-                    <p>手机：13967247869</p>
-                    <p>QQ：153009741</p></td>
-            </tr>
-            </tbody>
-        </table>
-
-    </div>
-    <div class="footer">
-        <p><a href="/html/list_189.html">关于本站</a><span> | </span><a
-                    href="/html/list_190.html">版权声明</a><span> | </span><a
-                    href="/html/list_247.html">本站购物</a><span> | </span><a
-                    href="/html/list_249.html">展会合作</a><span> | </span><a
-                    href="/html/list_250.html">免责声明</a><span> | </span><a
-                    href="/html/list_191.html">联系方式</a><span> | </span><a
-                    href="/html/list_289.html">友情链接</a><span>  </span>
-
-        </p>
-        <p class="copyright">六安瓜片网 www.guapian.cn</p>
-
-    </div>
-
-</div>
-
-</body>
-</html>
+@endsection
